b15e99392559a4e334766dc28adbf00e842cd846
Nguyen Huy <1451010068huy@ou.edu.vn>
2020-07-27 11:14:27 +0700

    origin

--- saleapp/dao.py
+++ saleapp/dao.py
@@ -17,5 +17,36 @@ def read_products(keyword=None, from_price=None, to_price=None):
 def read_product_by_category_id(category_id):
     return [product for product in read_products() if product["category_id"] == category_id]
 
+def read_categories():
+    with open(os.path.join(app.root_path, "data/categories.json"), encoding="utf-8") as f:
+        return json.load(f)
+
+def read_product_by_id(product_id):
+    products = read_products()
+    for p in products:
+        if p.id == product_id:
+            return p
+    return None
+
+
+def add_products(name, description, price, image, category):
+    product = read_products()
+    product.append({
+            "id": len(product) + 1,
+            "name": name,
+            "description": description,
+            "price": float(price),
+            "image": image,
+            "category_id": int(category)
+        })
+    try:
+        with open(os.path.join(app.root_path, "data/products.json"), "w", encoding="utf-8") as f:
+            json.dump(product, f, ensure_ascii=False, indent=4)
+
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
+
 if __name__ == "__main__":
     print(read_products())
--- saleapp/main.py
+++ saleapp/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from saleapp import app
 from saleapp import dao
 
@@ -15,6 +15,25 @@ def product_list():
 def product_by_cate_id(cate_id):
     return render_template("product-list.html", products=dao.read_product_by_category_id(category_id=cate_id))
 
+@app.route("/products/add", methods=["get", "post"])
+def product_add():
+
+    err_msg = None
+    if request.method.lower() == "post":
+        if dao.add_products(**dict(request.form)):
+            return redirect(url_for('product-add.html'))
+        else:
+            err_msg = "Wrong!"
+
+        categories = dao.read_categories()
+        product = None
+        if request.args.get("product_id"):
+            product = dao.read_product_by_id(product_id=request.args("product_id"))
+
+        return redirect(url_for('product-add.html'))
+
+    categories = dao.read_categories()
+    return render_template("product-add.html", categories=categories)
 
 if __name__ == "__main__":
     app.run(debug=True)
